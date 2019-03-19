6e60a26c4bf1388bcdadd1646b9e872511d27401
mahima <mahima@qburst.com>
2019-03-19 18:38:56 +0530

    js change

--- paintAPI/checkerboard.js
+++ paintAPI/checkerboard.js
@@ -11,12 +11,11 @@ class CheckerboardPainter {
 
         const size = 32;
         for (let y = 0; y < geom.height / size; y++) {
-            console.log(geom.height);
             for (let x = 0; x < geom.width / size; x++) {
                 const color = colors[(x + y) % colors.length];
                 ctx.beginPath();
                 ctx.fillStyle = color;
-                ctx.rect(x * size, y * size, size, size);
+                ctx.rect(x * (size + spacing), y * (size +spacing), size, size);
                 ctx.fill();
             }
         }
