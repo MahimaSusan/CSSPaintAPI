4e28ca774bcd754d76a3dca7bb47962af0055413
mahima <mahima@qburst.com>
2019-03-28 14:38:03 +0530

    change

--- demo/scripts/scripts.js
+++ demo/scripts/scripts.js
@@ -58,10 +58,10 @@ class SuperUnderline {
             return Math.floor(Math.random() * (max - min + 1)) + min
         }
         // values to set for the paint
-        const numUnderlines = 3
-        const spread = 20
-        ctx.lineWidth = 3
-        ctx.strokeStyle = ‘red’
+        const numUnderlines = 3;
+        const spread = 20;
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'black';
         for (let i = 0; i < numUnderlines; i++) {
             ctx.beginPath()
             ctx.moveTo(0, getRandom(0, spread) + size.height / 1.4)
