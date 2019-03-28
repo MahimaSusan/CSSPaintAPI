a02b3b0e250545c06b0385e9cbf90a8eb3b1675e
mahima <mahima@qburst.com>
2019-03-28 14:20:00 +0530

    change

--- demo/scripts/scripts.js
+++ demo/scripts/scripts.js
@@ -1,28 +1,28 @@
-// // checkerboard.js
-// class CheckerboardPainter {
+// checkerboard.js
+class CheckerboardPainter {
 
 
-//     static get inputProperties() { return ['--checkerboard-spacing', '--checkerboard-size']; }
-//     paint(ctx, geom, properties) {
-//         // Use `ctx` as if it was a normal canvas
-//         const size = parseInt(properties.get('--checkerboard-size').toString());
-//         const spacing = parseInt(properties.get('--checkerboard-spacing').toString());
-//         const colors = ['red', 'green', 'blue'];
+    static get inputProperties() { return ['--checkerboard-spacing', '--checkerboard-size']; }
+    paint(ctx, geom, properties) {
+        // Use `ctx` as if it was a normal canvas
+        const size = parseInt(properties.get('--checkerboard-size').toString());
+        const spacing = parseInt(properties.get('--checkerboard-spacing').toString());
+        const colors = ['red', 'green', 'blue'];
 
-//         for (let y = 0; y < geom.height / size; y++) {
-//             for (let x = 0; x < geom.width / size; x++) {
-//                 const color = colors[(x + y) % colors.length];
-//                 ctx.beginPath();
-//                 ctx.fillStyle = color;
-//                 ctx.rect(x * (size + spacing), y * (size +spacing), size, size);
-//                 ctx.fill();
-//             }
-//         }
-//     }
-// }
+        for (let y = 0; y < geom.height / size; y++) {
+            for (let x = 0; x < geom.width / size; x++) {
+                const color = colors[(x + y) % colors.length];
+                ctx.beginPath();
+                ctx.fillStyle = color;
+                ctx.rect(x * (size + spacing), y * (size +spacing), size, size);
+                ctx.fill();
+            }
+        }
+    }
+}
 
-// // Register our class under a specific name
-// registerPaint('checkerboard', CheckerboardPainter);
+// Register our class under a specific name
+registerPaint('checkerboard', CheckerboardPainter);
 
 
 // class SuperUnderline {
@@ -46,21 +46,21 @@
 // }
 // registerPaint('superUnderline', SuperUnderline)
 
-class Shape {
-  paint(ctx, geom, properties) {
+// class Shape {
+//   paint(ctx, geom, properties) {
     
-    let x = geom.width/2;
-    let y = geom.height/2;
+//     let x = geom.width/2;
+//     let y = geom.height/2;
 
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 4;
-    ctx.beginPath();
-    ctx.arc(x, y, 50, 0, 2*Math.PI);
-    ctx.stroke();
-    ctx.closePath();
+//     ctx.strokeStyle = 'white';
+//     ctx.lineWidth = 4;
+//     ctx.beginPath();
+//     ctx.arc(x, y, 50, 0, 2*Math.PI);
+//     ctx.stroke();
+//     ctx.closePath();
     
-  }
-}
+//   }
+// }
 
-// Register our class under a specific name
-registerPaint('awesomePattern', Shape);
+// // Register our class under a specific name
+// registerPaint('awesomePattern', Shape);
