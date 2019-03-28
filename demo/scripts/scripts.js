b25950626250667c25dbbd2c12bc48ac3b7309d3
mahima <mahima@qburst.com>
2019-03-28 14:24:14 +0530

    change

--- demo/scripts/scripts.js
+++ demo/scripts/scripts.js
@@ -23,3 +23,27 @@ class CheckerboardPainter {
 
 // Register our class under a specific name
 registerPaint('checkerboard', CheckerboardPainter);
+
+
+
+
+
+
+class Shape {
+  paint(ctx, geom, properties) {
+    
+    let x = geom.width/2;
+    let y = geom.height/2;
+
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+    
+  }
+}
+
+// Register our class under a specific name
+registerPaint('awesomePattern', Shape);
