aab5bf9b11d53a04b1f9abbc6d9d6d18213bc367
mahima <mahima@qburst.com>
2019-10-24 13:11:47 +0530

    custom properties

--- demo/scripts/scripts.js
+++ demo/scripts/scripts.js
@@ -77,33 +77,10 @@ class CheckerboardPainter {
 // Register our class under a specific name
 registerPaint('checkerboard', CheckerboardPainter);
 
-class BarChartPainter {
-    paint(ctx, {width, height}, props) {
-      const gap = parseInt(
-        (props.get('--bar-gap') || 10).toString(),
-        10
-      );
-      const data = this._parseData(props.get('--bar-map'));
-      const max = this._getMax(data);
-      const multiplier = height / max;
-      const barW = (width - (gap * (data.length - 1))) / data.length;
 
-      for (let i = 0; i < data.length; i++) {
-        const x = i * (barW + gap);
-        const barHeight = data[i].value * multiplier;
-        const y = height - barHeight;
+//Custom Properties
 
-        ctx.fillStyle = data[i].color;
-
-        ctx.fillRect(x, y, barW, barHeight);
-      }
-    }
-  }
-  registerPaint('bar-chart', BarChartPainter);
-
-  //Custom Properties
-
-  class HeaderHighlight {
+class HeaderHighlight {
 
 
     static get inputProperties() { return ['--boxColor', '--widthSubtractor']; }
